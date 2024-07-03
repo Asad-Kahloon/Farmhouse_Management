7a2a02b3d1037dc6e5d45b5ec4913c15128c1e36
Asad-Kahloon <asadkahloon77@gmail.com>
2024-07-03 16:51:40 +0500

    register functionality completed

---
+++ server/routes/admin.js
@@ -0,0 +1,63 @@
+import bcrypt from "bcrypt";
+import express from "express";
+import dotenv from "dotenv";
+import multer from "multer";
+
+import { Admin } from "../models/Admin.js";
+
+dotenv.config();
+
+// Configure multer for file upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/admins"); // Set your upload destination
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Set file name
+  },
+});
+const upload = multer({ storage });
+
+const router = express.Router();
+
+// staff member registration
+
+router.post("/register", upload.single("photo"), async (req, res) => {
+  try {
+    const { name, cnic, email, password, gender } = req.body;
+    const photo = req.file; // Uploaded image file
+    const aname = await Admin.findOne({ $or: [{ cnic }, { email }] });
+    if (aname) {
+      if (aname.cnic === cnic) {
+        return res.json({
+          cnic: true,
+          message: "Account Already Registered On This CNIC",
+        });
+      }
+      if (aname.email === email) {
+        return res.json({
+          email: true,
+          message: "Account Already Registered On This Email",
+        });
+      }
+    } else {
+      const hashPassword = await bcrypt.hash(password, 10);
+      const newAdmin = new Admin({
+        name,
+        gender,
+        cnic,
+        email,
+        password: hashPassword,
+        role: "staff",
+        photo: photo.filename,
+      });
+      await newAdmin.save();
+      return res.json({ admin_registered: true });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ message: "Error Creating Admin" });
+  }
+});
+
+export { router as AdminRouter };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -39,4 +39,21 @@ router.post("/login", async (req, res) => {
   }
 });
 
-export { router as AdminRouter };
+const verifyAdmin = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json({ message: "Invalid Admin" });
+  } else {
+    jwt.verify(token, process.env.Admin_Key, (err, decoded) => {
+      if (err) {
+        return res.json({ message: "Invalid Token" });
+      } else {
+        req.email = decoded.email;
+        req.role = decoded.role;
+        next();
+      }
+    });
+  }
+};
+
+export { router as AuthRouter, verifyAdmin };
